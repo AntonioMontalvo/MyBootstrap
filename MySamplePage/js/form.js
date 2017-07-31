66e4d195bc7d2386817afffa794b4b0f07e330bc
Antonio Montalvo <montalvocode@yahoo.com>
2017-07-31 11:25:04 -0700

    add first files

---
+++ MySamplePage/js/form.js
@@ -0,0 +1,10 @@
+console.log('Hello Boostrap Form');
+$("#explain").hide();
+$('#optionsRadios2').click(function(){
+	$("#explain").show();
+});
+
+$('#optionsRadios1').click(function(){
+	$("#explain").hide();
+})
+
